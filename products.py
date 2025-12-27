147ea6c163be5237a6ac323081c9fccfea726ad8
a0982919191-cyber <a0982919191@gmail.com>
2025-12-28 00:43:27 +0800

    Add files via upload

--- products.py
+++ products.py
@@ -5,11 +5,11 @@ PRODUCT_CATALOG = {
     "團體服系列": {
         "AG21000 重磅棉T": {
             "name": "AG21000 重磅棉T",
-            
+
             # 1. 顏色選項 (顯示在選單上的名稱)
             "colors": [
-                "白 (White)", 
-                "黑 (Black)", 
+                "白 (White)",
+                "黑 (Black)",
                 "丈青 (Navy)",
                 "麻灰 (HeatherGray)",
                 "炭灰 (CharcoalGray)",
@@ -27,11 +27,10 @@ PRODUCT_CATALOG = {
                 "蜜桃橘 (PeachOrange)",
                 "淺粉 (LightPink)",
                 "卡其 (Khaki)",
-                "米褐 (BeigeBrown)"
+                "米褐 (BeigeBrown)",
             ],
-            
+
             # 2. 顏色對應表 (顯示名稱 -> 檔名關鍵字)
-            # 這裡的英文必須跟您 assets 資料夾裡的檔名一模一樣 (區分大小寫)
             "color_map": {
                 "白 (White)": "White",
                 "黑 (Black)": "Black",
@@ -52,136 +51,100 @@ PRODUCT_CATALOG = {
                 "蜜桃橘 (PeachOrange)": "PeachOrange",
                 "淺粉 (LightPink)": "LightPink",
                 "卡其 (Khaki)": "Khaki",
-                "米褐 (BeigeBrown)": "BeigeBrown"
+                "米褐 (BeigeBrown)": "BeigeBrown",
             },
-            
+
             # 3. 檔名開頭 (圖片必須放在 assets 資料夾內)
             "image_base": "AG21000",
-            
+
             # 4. 正面印刷位置
             "pos_front": {
                 "正中間 (Center)": {"coords": (380, 270)},
-                "左胸 (Left Chest)": {"coords": (450, 230)},  
-                "右胸 (Right Chest)": {"coords": (300, 230)}, 
-                "左臂 (Left Sleeve)": {"coords": (720, 150), "default_rot": 55}, 
-                "右臂 (Right Sleeve)": {"coords": (50, 180), "default_rot": -45}   
+                "左胸 (Left Chest)": {"coords": (450, 230)},
+                "右胸 (Right Chest)": {"coords": (300, 230)},
+                "左臂 (Left Sleeve)": {"coords": (720, 150), "default_rot": 55},
+                "右臂 (Right Sleeve)": {"coords": (50, 180), "default_rot": -45},
             },
-            
+
             # 5. 背面印刷位置
             "pos_back": {
                 "背後正中 (Center)": {"coords": (500, 320)},
                 "左臂-後 (L.Sleeve Back)": {"coords": (950, 240)},
-                "右臂-後 (R.Sleeve Back)": {"coords": (80, 270)}
-            }
-        }
-    }
-    "CP101 吸濕排汗團體服": {
-    "image_base": "cp101",
-    "colors": [
-        "白色","淺灰色","深灰色","黑色","粉紅色",
-        "玫紅色","紅色","酒紅色","水藍色","湖藍色",
-        "寶藍色","藏青色","海藍色","草綠色","墨綠色",
-        "卡其色","淺黃色","黃色","淺紫色","深紫色",
-        "螢光粉色","螢光橘色","螢光綠色","螢光黃色",
+                "右臂-後 (R.Sleeve Back)": {"coords": (80, 270)},
+            },
+        },
 
-        "奶茶色","蓮藕粉色","玫瑰粉色","瑪瑙紅色",
-        "芥末黃色","金色","南瓜橘色","珊瑚橘色",
-        "天空藍色","薰衣草色","星空灰色","鯨魚藍色",
-        "翡翠綠色","軍綠色","駝色","太妃糖色"
-    ],
+        "CP101 吸濕排汗團體服": {
+            "name": "CP101 吸濕排汗團體服",
+            "image_base": "CP101",
 
-    "color_map": {
-        "白色": "white",
-        "淺灰色": "lightgray",
-        "深灰色": "darkgray",
-        "黑色": "black",
-        "粉紅色": "pink",
-        "玫紅色": "rose",
-        "紅色": "red",
-        "酒紅色": "wine",
-        "水藍色": "skyblue",
-        "湖藍色": "lakeblue",
-        "寶藍色": "royalblue",
-        "藏青色": "navy",
-        "海藍色": "seablue",
-        "草綠色": "grassgreen",
-        "墨綠色": "darkgreen",
+            "colors": [
+                "白色","淺灰色","深灰色","黑色","粉紅色",
+                "玫紅色","紅色","酒紅色","水藍色","湖藍色",
+                "寶藍色","藏青色","海藍色","草綠色","墨綠色",
+                "卡其色","淺黃色","黃色","淺紫色","深紫色",
+                "螢光粉色","螢光橘色","螢光綠色","螢光黃色",
+                "奶茶色","蓮藕粉色","玫瑰粉色","瑪瑙紅色",
+                "芥末黃色","金色","南瓜橘色","珊瑚橘色",
+                "天空藍色","薰衣草色","星空灰色","鯨魚藍色",
+                "翡翠綠色","軍綠色","駝色","太妃糖色"
+            ],
 
-        "卡其色": "khaki",
-        "淺黃色": "lightyellow",
-        "黃色": "yellow",
-        "淺紫色": "lavender",
-        "深紫色": "purple",
+            # 檔名對應：建議 assets 檔名格式
+            # CP101_<code>_front.png / CP101_<code>_back.png
+            "color_map": {
+                "白色": "White",
+                "淺灰色": "LightGray",
+                "深灰色": "DarkGray",
+                "黑色": "Black",
+                "粉紅色": "Pink",
+                "玫紅色": "Rose",
+                "紅色": "Red",
+                "酒紅色": "Wine",
+                "水藍色": "SkyBlue",
+                "湖藍色": "LakeBlue",
+                "寶藍色": "RoyalBlue",
+                "藏青色": "Navy",
+                "海藍色": "SeaBlue",
+                "草綠色": "GrassGreen",
+                "墨綠色": "DarkGreen",
+                "卡其色": "Khaki",
+                "淺黃色": "LightYellow",
+                "黃色": "Yellow",
+                "淺紫色": "LightPurple",
+                "深紫色": "DarkPurple",
+                "螢光粉色": "NeonPink",
+                "螢光橘色": "NeonOrange",
+                "螢光綠色": "NeonGreen",
+                "螢光黃色": "NeonYellow",
+                "奶茶色": "MilkTea",
+                "蓮藕粉色": "LotusPink",
+                "玫瑰粉色": "RosePink",
+                "瑪瑙紅色": "AgateRed",
+                "芥末黃色": "Mustard",
+                "金色": "Gold",
+                "南瓜橘色": "Pumpkin",
+                "珊瑚橘色": "Coral",
+                "天空藍色": "Sky",
+                "薰衣草色": "LavenderBlue",
+                "星空灰色": "SpaceGray",
+                "鯨魚藍色": "WhaleBlue",
+                "翡翠綠色": "Emerald",
+                "軍綠色": "Army",
+                "駝色": "Camel",
+                "太妃糖色": "Toffee",
+            },
 
-        "螢光粉色": "neonpink",
-        "螢光橘色": "neonorange",
-        "螢光綠色": "neongreen",
-        "螢光黃色": "neonyellow",
+            "pos_front": {
+                "正中間 (Center)": {"coords": (300, 360)},
+                "左胸 (Left Chest)": {"coords": (220, 340)},
+                "右胸 (Right Chest)": {"coords": (380, 340)},
+            },
 
-        "奶茶色": "milktea",
-        "蓮藕粉色": "lotuspink",
-        "玫瑰粉色": "rosepink",
-        "瑪瑙紅色": "agate",
-        "芥末黃色": "mustard",
-        "金色": "gold",
-        "南瓜橘色": "pumpkin",
-        "珊瑚橘色": "coral",
-
-        "天空藍色": "sky",
-        "薰衣草色": "lavenderblue",
-        "星空灰色": "spacegray",
-        "鯨魚藍色": "whaleblue",
-
-        "翡翠綠色": "emerald",
-        "軍綠色": "army",
-        "駝色": "camel",
-        "太妃糖色": "toffee"
-    },
-
-    "pos_front": {
-        "胸口置中": {"coords": (300, 360)},
-        "左胸小字": {"coords": (220, 340)},
-        "右胸小字": {"coords": (380, 340)}
-    },
-
-    "pos_back": {
-        "背中置中": {"coords": (300, 360)},
-        "上背字樣": {"coords": (300, 280)}
+            "pos_back": {
+                "背中置中 (Center)": {"coords": (300, 360)},
+                "上背字樣 (Upper Back)": {"coords": (300, 280)},
+            },
+        },
     }
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
