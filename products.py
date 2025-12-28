f0a84e21fc16059c091d9685df6fd3222a6e3442
a0982919191-cyber <a0982919191@gmail.com>
2025-12-28 23:07:06 +0800

    Update products.py

--- products.py
+++ products.py
@@ -143,20 +143,21 @@ PRODUCT_CATALOG = {
 
             "pos_front": {
                 "正中間 (Center)": {"coords": (590, 400)},
-                "左胸 (Left Chest)": {"coords": (700, 400)},
-                "右胸 (Right Chest)": {"coords": (480, 400)},
+                "左胸 (Left Chest)": {"coords": (740, 400)},
+                "右胸 (Right Chest)": {"coords": (440, 400)},
                 "左臂 (Left Sleeve)": {"coords": (1120, 200), "default_rot": 55},
-                "右臂 (Right Sleeve)": {"coords": (120, 200), "default_rot": -40},
+                "右臂 (Right Sleeve)": {"coords": (120, 200), "default_rot": -50},
             },
 
             "pos_back": {
                 "背中置中 (Center)": {"coords": (530, 350)},
-                "左臂-後 (L.Sleeve Back)": {"coords": (1020, 350)},
-                "右臂-後 (R.Sleeve Back)": {"coords": (120, 350)},
+                "左臂-後 (L.Sleeve Back)": {"coords": (900, 250)},
+                "右臂-後 (R.Sleeve Back)": {"coords": (50, 250)},
             },
         },
     }
 }
+
 
 
 
